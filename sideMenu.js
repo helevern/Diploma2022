71bcc2b7cf4b6ce08b747990b4e656f77c30106b
Daniil <helevern@gmail.com>
2022-06-08 15:28:49 +0400

    all

---
+++ sideMenu.js
@@ -0,0 +1,40 @@
+const burgerMenu = document.querySelector(".burgerMenu");
+
+const sideMenuContainer = document.querySelector(".sideMenuContainer");
+const OPEN_SIDEMENU_CLASSNAME = "openSideMenu";
+
+let isModalOpen = false;
+
+function toggleModal () {
+    sideMenuContainer.classList.toggle(OPEN_SIDEMENU_CLASSNAME);
+    isModalOpen = !isModalOpen;
+    if (isModalOpen){
+        document.body.style.overflow = "hidden";
+        sideMenuContainer.focus();
+    } else {
+        document.body.style.overflow = "auto";
+    }
+}
+
+const overlay = document.querySelector(".sideMenuOverlay")
+
+function toggleModalHandler(e) {
+    toggleModal();
+}
+
+const selectedCity = document.querySelector(".burgerMenu");
+burgerMenu.addEventListener("click", function (e) {
+    toggleModal();
+
+    if (isModalOpen) {
+        overlay.addEventListener("click", toggleModalHandler)
+    } else {
+        overlay.removeEventListener("click", toggleModalHandler)
+    }
+});
+
+window.addEventListener("keydown", function (e) {
+    if (e.keyCode === 27 && isModalOpen) {
+        toggleModal();
+    }
+})
